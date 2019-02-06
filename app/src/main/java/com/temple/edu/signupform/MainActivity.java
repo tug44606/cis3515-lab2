1afdef342f5c0b8206bd179bc7aa4fd23053bbbd
tug44606 <tug44606@temple.edu>
2019-02-05 22:53:51 -0500

    added Toasts for Save Click Event

--- app/src/main/java/com/temple/edu/signupform/MainActivity.java
+++ app/src/main/java/com/temple/edu/signupform/MainActivity.java
@@ -1,5 +1,6 @@
 package com.temple.edu.signupform;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,16 +18,53 @@ public class MainActivity extends AppCompatActivity {
         // For click event to save info
         Button saveInfo = findViewById(R.id.button);
 
-        // Getting user information
-        EditText email = findViewById(R.id.email);
-        EditText username = findViewById(R.id.username);
-        EditText password = findViewById(R.id.password);
-        EditText passConfirmation = findViewById(R.id.passwordconfirm);
 
-        // Converting text to strings
-        final String emailS = email.getText().toString();
-        final String usernameS = username.getText().toString();
-        final String passwordS = password.getText().toString();
-        final String passConfirmationS = passConfirmation.getText().toString();
+        saveInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Test
+                System.out.println("Button has been clicked");
+
+                // Getting user information
+                EditText emailAddress = findViewById(R.id.emailAddress);
+                System.out.println(emailAddress);
+                EditText username = findViewById(R.id.username);
+                System.out.println(username);
+                EditText password = findViewById(R.id.password);
+                System.out.println(password);
+                EditText passConfirmation = findViewById(R.id.passwordconfirm);
+                System.out.println(passConfirmation);
+
+                // Converting text to strings
+                String emailAddressS = emailAddress.getText().toString();
+                System.out.println(emailAddressS);
+                String usernameS = username.getText().toString();
+                System.out.println(usernameS);
+                String passwordS = password.getText().toString();
+                System.out.println(passwordS);
+                String passConfirmationS = passConfirmation.getText().toString();
+                System.out.println(passConfirmationS);
+
+                // If any empty fields
+                if((emailAddressS.isEmpty() && usernameS.isEmpty() && passwordS.isEmpty() && passConfirmationS.isEmpty()) == true){
+                    // Display toast
+                    Context c = getApplicationContext();
+                    Toast.makeText(c, "Please enter all fields!", Toast.LENGTH_SHORT).show();
+                }
+                // If passwords don't match
+                else if(!(passwordS.equals(passConfirmationS))){
+                    // Display toast
+                    Context c = getApplicationContext();
+                    Toast.makeText(c, "Passwords do not match!", Toast.LENGTH_SHORT).show();
+                }
+                // Default
+                else{
+                    // Display toast
+                    Context c = getApplicationContext();
+                    Toast.makeText(c, "Welcome, " + usernameS + ", to the SignUp Form App!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
